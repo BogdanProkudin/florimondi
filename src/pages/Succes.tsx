e111c1fad4438ab160cd01f71d87e00f0b8d40c7
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-02 14:27:40 +0200

    last

--- src/pages/Succes.tsx
+++ src/pages/Succes.tsx
@@ -1,5 +1,18 @@
 import { CheckCircle } from "lucide-react";
+import axios from "axios";
+import { useEffect } from "react";
+
 export const Success = () => {
+  useEffect(() => {
+    console.log("useEfffect");
+
+    async function sendRequst() {
+      console.log("request");
+
+      await axios.get("http://localhost:3000/result");
+    }
+    sendRequst();
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-green-100 text-green-700">
       <CheckCircle size={80} className="text-green-600 mb-4" />
