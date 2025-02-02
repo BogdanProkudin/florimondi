ffd45c3957cea3eae3985ed4539ff3dfef6cca2d
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-02 06:23:39 +0200

    last

--- src/pages/Fail.tsx
+++ src/pages/Fail.tsx
@@ -1,3 +1,12 @@
+import { XCircle } from "lucide-react";
 export const Fail = () => {
-  return <div>Fail</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-red-100 text-red-700">
+      <XCircle size={80} className="text-red-600 mb-4" />
+      <h1 className="text-3xl font-bold">Оплата не прошла!</h1>
+      <p className="mt-2 text-lg">
+        Попробуйте снова или свяжитесь с поддержкой.
+      </p>
+    </div>
+  );
 };
--- src/pages/Succes.tsx
+++ src/pages/Succes.tsx
@@ -1,3 +1,10 @@
+import { CheckCircle } from "lucide-react";
 export const Success = () => {
-  return <div>SUCCESS1</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-green-100 text-green-700">
+      <CheckCircle size={80} className="text-green-600 mb-4" />
+      <h1 className="text-3xl font-bold">Оплата прошла успешно!</h1>
+      <p className="mt-2 text-lg">Спасибо за вашу покупку.</p>
+    </div>
+  );
 };
