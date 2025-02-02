bbfb36654da2b1d9cfb19a050df8701e3a3ade40
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-02 05:46:31 +0200

    last

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import "core-js/actual/structured-clone";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/florimondi/",
+  define: {},
 });
